a82cc8c9589ee34dd018f8c827470fbf6e343576
Adin355 <adincaspary@gmail.com>
2024-05-07 22:20:45 -0400

    testmoter added

---
+++ testmoter.py
@@ -0,0 +1,23 @@
+
+import RPi.GPIO as GPIO
+
+# Motor 1
+Motor1F = 11
+Motor1B = 13
+
+# Motor 2
+Motor2F = 29 
+Motor2B = 31
+
+GPIO.setmode(GPIO.BOARD)
+# Motor 1
+GPIO.setup(Motor1F, GPIO.OUT)
+GPIO.setup(Motor1B, GPIO.OUT)
+# Motor 2
+GPIO.setup(Motor2F, GPIO.OUT)
+GPIO.setup(Motor2B, GPIO.OUT)
+
+GPIO.output(Motor1F, GPIO.HIGH)
+GPIO.output(Motor2F, GPIO.HIGH)
+
+GPIO.cleanup()
